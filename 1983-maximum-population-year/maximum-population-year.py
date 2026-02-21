c2208fbf87d0bdd1740d8b66b59ce2ddbec58607
Habib Ghulam Bheek <habibghulam066@gmail.com>
2026-02-21 11:25:46 +0500

    Time: 19 ms (9.65%) | Memory: 12.6 MB (33.33%) - LeetSync

--- 1983-maximum-population-year/maximum-population-year.py
+++ 1983-maximum-population-year/maximum-population-year.py
@@ -4,9 +4,8 @@ class Solution(object):
         :type logs: List[List[int]]
         :rtype: int
         """
-        logs.sort()
+        # logs.sort()
         # print(logs)
-        n  = len(logs)
         # curr_pop = 1
         # max_pop  = 1
         # # extreme boundary
@@ -21,17 +20,21 @@ class Solution(object):
         #         curr_pop = 1
         #         boundary = logs[i][1]
         # bruute force approach
+        n  = len(logs)
         curr_pop = 1
         max_pop = 0
         max_year = logs[0][0]
+    
         for i in range(n):
             curr_pop  = 0
             earliest_year = logs[i][0]
-
             for j in range(n):
                 if logs[i][0] >= logs[j][0] and logs[i][0] <= (logs[j][1] -1):
                     curr_pop += 1
                 if  curr_pop > max_pop:
                     max_pop = curr_pop
+                    # print(curr_pop, logs[i])
                     max_year = logs[i][0]
+                elif curr_pop == max_pop:
+                    max_year = min(max_year, logs[i][0])
         return max_year
