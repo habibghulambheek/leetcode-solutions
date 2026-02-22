8f8f51a51a895b00a7f626e22e4511d0cabb4167
Habib Ghulam Bheek <habibghulam066@gmail.com>
2026-02-22 12:31:08 +0500

    Time: 51 ms (30.36%) | Memory: 12.4 MB (92.53%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.py
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.py
@@ -4,21 +4,24 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        prefix = [0] * (n)
-        prefix[0] = 1
-        for i in range(1,n):
-            prefix[i] = prefix[i - 1] + (i + 1)
-        print(prefix)
-        left = 0
+        def prefix(s):
+            return (s*(s + 1))//2
+        # prefix = [0] * (n)
+        # prefix[0] = 1
+        # for i in range(1,n):
+        #     prefix[i] = prefix[i - 1] + (i + 1)
+        # print(prefix)
+        # left = 0
         # [1, 3, 6, 10]
         # 3 ==  4
-        right = prefix[n - 1] - prefix[0]
+        left = 0
+        right = prefix(n) - prefix(1)
         for i in range(n):
             if left == right:
                 return (i+1)
-            left  =  prefix[i]
+            left  =  prefix(i+1)
             if i != (n -1):
-                right =  prefix[n - 1] - prefix[i+1]
+                right =  prefix(n) - prefix(i+2)
             else:
                 right == 0
         return -1
