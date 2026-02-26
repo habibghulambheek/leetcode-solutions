c5cda6791738228bd872f0aadf842f4513f45c79
Habib Ghulam Bheek <habibghulam066@gmail.com>
2026-02-26 16:02:07 +0500

    Time: 40 ms (35.33%) | Memory: 15.2 MB (29.33%) - LeetSync

--- 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.py
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.py
@@ -5,15 +5,13 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        prefix = {0:0}
-        n = len(nums)
+        prefix = {0:1}
+        # n = len(nums)
         ctr = 0
         running_sum = 0
-        for j in range(n):
-            running_sum += nums[j]
+        for num in nums:
+            running_sum += num
             r_j = running_sum % k
             ctr += prefix.get(r_j, 0)
-            if r_j ==0:
-                ctr += 1
             prefix[r_j] = prefix.get(r_j,0) + 1
         return ctr
