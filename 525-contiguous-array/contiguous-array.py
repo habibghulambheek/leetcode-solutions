a0ef3cf8ce14bcdf02dcccd7aa5863fa038f0b00
Habib Ghulam Bheek <habibghulam066@gmail.com>
2026-03-01 11:03:46 +0500

    Time: 127 ms (17.01%) | Memory: 16.7 MB (55.95%) - LeetSync

---
+++ 525-contiguous-array/contiguous-array.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def findMaxLength(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n =  len(nums)
+        running_balance = 0
+        prefix = {0:0}
+        ans = 0
+        
+        for i in range(n):
+            if nums[i] == 1:
+                running_balance = running_balance + 1 
+            else:
+                running_balance = running_balance - 1 
+            if running_balance == 0:
+                ans  = i + 1
+            elif running_balance in prefix:
+                ans = max(ans,i - prefix[running_balance])
+            if running_balance not in prefix:
+                prefix[running_balance] = i
+        return ans
