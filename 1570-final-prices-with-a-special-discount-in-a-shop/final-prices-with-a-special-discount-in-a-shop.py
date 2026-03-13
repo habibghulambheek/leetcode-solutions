5606583e3e4fc052f8cd9e687f397f0e1b2f468f
Habib Ghulam Bheek <habibghulam066@gmail.com>
2026-03-13 15:32:58 +0500

    Time: 0 ms (100.00%) | Memory: 12.4 MB (95.70%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.py
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.py
@@ -7,14 +7,15 @@ class Solution(object):
         stack = []
 
         n = len(prices)
-        ans = [0] * n
+        # ans = [0] * n
         for j in range(n-1,-1,-1 ):
             while stack and stack[-1] > prices[j]:
                 stack.pop()
-            if not stack:
-                ans[j] = prices[j]
-            else:
-                ans[j] = prices[j] - stack[-1]
+            # if not stack:
+            #     ans[j] = prices[j]
+            value = prices[j]
+            if stack:
+                prices[j] = prices[j] - stack[-1]
             
-            stack.append(prices[j])
-        return ans
+            stack.append(value)
+        return prices
