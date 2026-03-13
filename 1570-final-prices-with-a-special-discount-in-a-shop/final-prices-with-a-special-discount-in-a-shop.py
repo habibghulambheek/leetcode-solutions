a4a8107871587a80491b3d6860490d2d356700f2
Habib Ghulam Bheek <habibghulam066@gmail.com>
2026-03-13 15:31:14 +0500

    Time: 3 ms (73.80%) | Memory: 12.2 MB (99.59%) - LeetSync

---
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def finalPrices(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: List[int]
+        """
+        stack = []
+
+        n = len(prices)
+        ans = [0] * n
+        for j in range(n-1,-1,-1 ):
+            while stack and stack[-1] > prices[j]:
+                stack.pop()
+            if not stack:
+                ans[j] = prices[j]
+            else:
+                ans[j] = prices[j] - stack[-1]
+            
+            stack.append(prices[j])
+        return ans
