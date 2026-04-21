a05e62e5360eb923c211203f053176d8fc4367ec
Habib Ghulam Bheek <habibghulam066@gmail.com>
2026-04-21 14:51:37 +0500

    Time: 132 ms (14.65%) | Memory: 31.2 MB (10.10%) - LeetSync

---
+++ 456-132-pattern/132-pattern.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def find132pattern(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        stack = []
+        mid  = None
+        n = len(nums)
+        for i in range(n-1,-1,-1):
+            while stack and stack[-1] < nums[i]:
+                value = stack.pop()
+                if mid == None or mid < value:
+                    mid = value
+            if stack and stack[-1] > nums[i] and mid != None and mid > nums[i]:
+                return True
+            stack.append(nums[i])
+        return False
