0dbf89cf4b603f8934fe8f0699179ecee6be8ffd
Habib Ghulam Bheek <habibghulam066@gmail.com>
2026-05-01 13:01:58 +0500

    Time: 126 ms (68.00%) | Memory: 23.1 MB (28.00%) - LeetSync

--- 1792-find-the-most-competitive-subsequence/find-the-most-competitive-subsequence.py
+++ 1792-find-the-most-competitive-subsequence/find-the-most-competitive-subsequence.py
@@ -8,14 +8,16 @@ class Solution(object):
         stack  = []
         # [84,10,71,23,66,61,62,64,34,41,80,25,91,43,4,75,65,13,37,41,46,90,55,8,85,61,95,71]
         n = len(nums) 
+        remove = n - k
         for i in range(n):
             # print("Req elements:",(k - len(stack)) ,"Left:", n - (i), "Stack:", stack)
                
-            while stack and stack[-1] > nums[i] and (k - len(stack)) < n - (i):
+            while stack and stack[-1] > nums[i] and remove > 0:
                     stack.pop()
+                    remove -= 1
             # print("After popping by ", nums[i] ,":", stack)
-            if len(stack) < k:
-                stack.append(nums[i])
+       
+            stack.append(nums[i])
             # print("After appending:", stack)
             
-        return stack
+        return stack[0:k]
