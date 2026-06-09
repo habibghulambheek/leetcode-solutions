5473a066dc09e78c41ac01964400ca14f858d0a4
Habib Ghulam Bheek <habibghulam066@gmail.com>
2026-06-09 10:46:27 +0500

    Time: 2 ms (19.85%) | Memory: 12.3 MB (56.27%) - LeetSync

--- 263-ugly-number/ugly-number.py
+++ 263-ugly-number/ugly-number.py
@@ -4,28 +4,35 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
-        if n <= 0 :
+        # if n <= 0 :
+        #     return False
+        # primes = {2,3,5,7,11,13,17,19,23,29,31}
+        # # n =  abs(n)
+        # def isPrime(x):
+        #     if x <= 31:
+        #         return x in primes
+        #     for i in range(2,int(math.ceil(x**(0.5)))):
+        #         if x % i == 0:
+        #             return False
+        #     return True
+        # acc_primes =  {2,3,5}
+        # # print(n**(0.5),n)
+        # print(int(math.ceil(n**(1/2))))
+        # for x in range(1,int(math.ceil(n**(0.5)))):
+        #     # print(n,x , n%x)
+        #     if n % x == 0:
+        #         f1 = x
+        #         f2 = n / x
+        #         # print(f1,f2)
+        #         if f1 not in acc_primes and isPrime(f1):          
+        #             return False
+        #         if f2 not in acc_primes and isPrime(f2):
+        #             return False
+        # return True
+        if n <= 0:
             return False
-        primes = {2,3,5,7,11,13,17,19,23,29,31}
-        # n =  abs(n)
-        def isPrime(x):
-            if x <= 31:
-                return x in primes
-            for i in range(2,int(math.ceil(x**(0.5)))):
-                if x % i == 0:
-                    return False
-            return True
-        acc_primes =  {2,3,5}
-        # print(n**(0.5),n)
-        print(int(math.ceil(n**(1/2))))
-        for x in range(1,int(math.ceil(n**(0.5)))):
-            # print(n,x , n%x)
-            if n % x == 0:
-                f1 = x
-                f2 = n / x
-                # print(f1,f2)
-                if f1 not in acc_primes and isPrime(f1):          
-                    return False
-                if f2 not in acc_primes and isPrime(f2):
-                    return False
-        return True
+        primes = [2,3,5]
+        for x in primes:
+            while n % x == 0:
+                n = n / x
+        return n ==1
