7eb907a05495be65689519d4c204e2b95f4879e0
Habib Ghulam Bheek <habibghulam066@gmail.com>
2026-06-30 09:47:38 +0500

    Time: 1191 ms (17.09%) | Memory: 33.7 MB (98.29%) - LeetSync

--- 502-ipo/ipo.py
+++ 502-ipo/ipo.py
@@ -11,14 +11,11 @@ class Solution(object):
         capital_profit = [(capital[i], profit[i]) for i in range(n)]
         heapq.heapify(capital_profit)
         profits = []
-        ans = 0
-        # print(capital_profit)
         while k > 0:
             while capital_profit and capital_profit[0][0] <= w:
                 heapq.heappush(profits, -heapq.heappop(capital_profit)[1])
-            # print(profits)
             if not profits:
-                return w
+                break
             w += -heapq.heappop(profits) 
             k -= 1
         return w
