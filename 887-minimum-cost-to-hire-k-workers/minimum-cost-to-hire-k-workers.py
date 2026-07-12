fc14eced3f1474e0c4b12465866d110f57a37eab
Habib Ghulam Bheek <habibghulam066@gmail.com>
2026-07-12 17:20:15 +0500

    Time: 104 ms (84.62%) | Memory: 14.5 MB (92.31%) - LeetSync

--- 887-minimum-cost-to-hire-k-workers/minimum-cost-to-hire-k-workers.py
+++ 887-minimum-cost-to-hire-k-workers/minimum-cost-to-hire-k-workers.py
@@ -1,23 +1,21 @@
+import heapq
+
 class Solution(object):
     def mincostToHireWorkers(self, quality, wage, k):
-        """
-        :type quality: List[int]
-        :type wage: List[int]
-        :type k: int
-        :rtype: float
-        """
         n = len(quality)
-        ratios =  sorted([(float(wage[i])/quality[i], quality[i]) for i in range(n)])
+        ratios = sorted([(float(wage[i])/quality[i], quality[i]) for i in range(n)])
 
         best_cost = float("inf")
         max_heap = []
-        quality_sum =  0
+        quality_sum = 0
 
         for ratio, q in ratios:
+            quality_sum += q
             heapq.heappush(max_heap, -q)
-            quality_sum += q   
+
+            if len(max_heap) > k:
+                quality_sum += heapq.heappop(max_heap)   # fixed: += not -=
             if len(max_heap) == k:
                 cost = ratio * quality_sum
                 best_cost = min(best_cost, cost)
-                quality_sum +=  heapq.heappop(max_heap)
         return best_cost
