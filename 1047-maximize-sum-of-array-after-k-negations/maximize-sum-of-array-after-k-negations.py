db9f503d062c0077dd937f2077e5938d61e8c0b1
Habib Ghulam Bheek <habibghulam066@gmail.com>
2026-07-21 14:38:39 +0500

    Time: 0 ms (100.00%) | Memory: 12.5 MB (44.69%) - LeetSync

--- 1047-maximize-sum-of-array-after-k-negations/maximize-sum-of-array-after-k-negations.py
+++ 1047-maximize-sum-of-array-after-k-negations/maximize-sum-of-array-after-k-negations.py
@@ -7,14 +7,12 @@ class Solution(object):
         """
         nums.sort()
         n = len(nums)
-        # print(nums)
         for i in range(n):
             if k <= 0:
                 break
             if nums[i] < 0:
-                # print("1")
-                nums[i] = -nums[i]
-                k -= 1
+                 nums[i] = -nums[i]
+                 k -= 1
             elif nums[i] == 0:
                 k = 0
             else:
@@ -22,21 +20,16 @@ class Solution(object):
                 if i != 0:
                     if nums[i] > nums[i-1]:
                         idx -= 1
-                # print("Here", k % 2)
                 if k % 2 == 0:
                     k = 0
 
                     continue
                 k = 0
     
-                # print("changed" )
                 nums[idx] = -nums[idx]
         idx = 1
         if k > 0:
-            # k -= 1
             if k % 2 != 0:
                 nums[-idx] = -nums[-idx]
-            # idx += 1
 
-        # print(nums)
         return sum(nums)
