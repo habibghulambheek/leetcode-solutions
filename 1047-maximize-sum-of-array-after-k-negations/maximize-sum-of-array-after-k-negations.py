9e86c911ef380f7f8a45f1805ebe4a9b65896717
Habib Ghulam Bheek <habibghulam066@gmail.com>
2026-07-21 15:25:03 +0500

    Time: 12 ms (15.93%) | Memory: 12.4 MB (78.32%) - LeetSync

--- 1047-maximize-sum-of-array-after-k-negations/maximize-sum-of-array-after-k-negations.py
+++ 1047-maximize-sum-of-array-after-k-negations/maximize-sum-of-array-after-k-negations.py
@@ -5,31 +5,8 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        nums.sort()
-        n = len(nums)
-        for i in range(n):
-            if k <= 0:
-                break
-            if nums[i] < 0:
-                 nums[i] = -nums[i]
-                 k -= 1
-            elif nums[i] == 0:
-                k = 0
-            else:
-                idx = i
-                if i != 0:
-                    if nums[i] > nums[i-1]:
-                        idx -= 1
-                if k % 2 == 0:
-                    k = 0
-
-                    continue
-                k = 0
-    
-                nums[idx] = -nums[idx]
-        idx = 1
-        if k > 0:
-            if k % 2 != 0:
-                nums[-idx] = -nums[-idx]
-
+        heapq.heapify(nums)
+        while k != 0:
+            heapq.heappush(nums, -heapq.heappop(nums))
+            k -= 1
         return sum(nums)
