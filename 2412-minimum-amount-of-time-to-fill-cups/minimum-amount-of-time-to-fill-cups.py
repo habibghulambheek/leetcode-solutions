0f953d408e0c4b8b2b9c712eaf73d190e58f3814
Habib Ghulam Bheek <habibghulam066@gmail.com>
2026-07-26 21:12:13 +0500

    Time: 3 ms (32.86%) | Memory: 12.3 MB (88.57%) - LeetSync

---
+++ 2412-minimum-amount-of-time-to-fill-cups/minimum-amount-of-time-to-fill-cups.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def fillCups(self, amount):
+        """
+        :type amount: List[int]
+        :rtype: int
+        """
+        ans = 0
+        while amount[0] != 0 or amount[1] != 0 or amount[2] != 0:
+            idx1, idx2 = None, None
+            if amount[0] > amount[1]:
+                idx1 = 0
+                idx2 = 1
+            else:
+                idx1 = 1
+                idx2 = 0
+            if amount[idx1] > amount[2]:
+                if amount[2] > amount[idx2]:
+                    idx2 = 2
+            else:
+                idx2 = idx1 
+                idx1 = 2
+            # print(amount, amount[idx1], amount[idx2])
+            if amount[idx1] != 0:
+                amount[idx1] -= 1
+            if amount[idx2] != 0:
+                amount[idx2] -= 1
+            ans += 1
+        return ans
+        
