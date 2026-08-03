fa0034e26e229f01c40cc16eda94684bcdcc8cb4
Habib Ghulam Bheek Fareed <habibghulam066@gmail.com>
2026-08-03 17:05:52 +0500

    Time: 19 ms (71.61%) | Memory: 14.7 MB (52.16%) - LeetSync

---
+++ 135-candy/candy.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def candy(self, ratings):
+        """
+        :type ratings: List[int]
+        :rtype: int
+        """
+        ans = 1
+        n = len(ratings)
+        ans =  [0]*n
+        ans[0] =  1
+        for i in range(1, n):
+            if ratings[i-1] < ratings[i]:
+                ans[i] = ans[i-1] + 1
+            else:
+                # print(ratings[i])
+                ans[i] =  1 
+        for i in range(n-2,-1,-1):
+            # print(ratings[i], ratings[i + 1])
+            if ratings[i] > ratings[i+1]:
+                if ans[i] < ans[i+1] +1:
+                    ans[i] = ans[i+1] + 1
+       
+        # print(ans, ratings)
+        return sum(ans)
