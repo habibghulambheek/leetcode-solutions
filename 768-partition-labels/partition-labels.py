33674592347fcb3a77a69981fd15867377f7af81
Habib Ghulam Bheek Fareed <habibghulam066@gmail.com>
2026-08-04 15:06:42 +0500

    Time: 6 ms (60.61%) | Memory: 12.3 MB (58.03%) - LeetSync

--- 768-partition-labels/partition-labels.py
+++ 768-partition-labels/partition-labels.py
@@ -6,13 +6,10 @@ class Solution(object):
         """
         
         last_idx = {}
-        # start_idx = {}
         n = len(s)
         for i in range(n-1,-1,-1):
-            # print(i)
             if s[i] not in last_idx:
                 last_idx[s[i]] = i
-        # print(last_idx)
         ans = []
         start, end = 0, last_idx[s[i]]
         for i in range(n):
