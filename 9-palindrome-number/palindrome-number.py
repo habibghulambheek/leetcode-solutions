a6e14759bbf6889d4a8c4872ca219ff81a15ea93
Habib Ghulam Bheek Fareed <habibghulam066@gmail.com>
2026-08-16 14:25:09 +0500

    Time: 7 ms (78.15%) | Memory: 12.4 MB (18.11%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0:
+            return False
+        x_str  = str(x)
+        return x_str[::-1] == x_str
+        
