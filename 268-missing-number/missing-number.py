6e5f63a98b1ecd3b5c177473617b60838d8bb38b
Habib Ghulam Bheek Fareed <habibghulam066@gmail.com>
2026-08-22 14:23:41 +0500

    Time: 7 ms (42.21%) | Memory: 13.3 MB (68.09%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -4,14 +4,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        ans = 0
         n = len(nums)
-
-        numbers = {i:False for i in range(n+1)}
-        
         for x in nums:
-            numbers[x] = True
-        
+            ans ^= x
         for x in range(n+1):
-            if numbers[x] != True:
-                return x
-            
+            ans ^= x
+        return ans
