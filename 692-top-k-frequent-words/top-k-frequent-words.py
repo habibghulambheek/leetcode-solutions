c9d8bf49a5b1db3dd6ad34fe6dcb0fde0c9c434a
Habib Ghulam Bheek Fareed <habibghulam066@gmail.com>
2026-09-20 14:22:42 +0500

    Time: 8 ms (6.61%) | Memory: 19.6 MB (20.28%) - LeetSync

--- 692-top-k-frequent-words/top-k-frequent-words.py
+++ 692-top-k-frequent-words/top-k-frequent-words.py
@@ -1,19 +1,29 @@
-class Reverse:
-    def __init__(self, word):
-        self.word = word
+from collections import Counter
+import heapq
+class Item:
+    def __init__(self,count, word):
+        self.count = count
+        self.word  = word
+        # print(word)
     def __lt__(self, other):
+        if self.count != other.count:
+            return self.count < other.count
         return self.word > other.word
-class Solution(object):
-    def topKFrequent(self, words, k):
-        freq = {}
-        for word in words:
-            freq[word] = freq.get(word, 0) + 1
-        heap = []
-        for word, count in freq.items():
-            heapq.heappush(heap, (count, Reverse(word)))
-            if len(heap) > k:
-                heapq.heappop(heap)
-        res = []
-        while heap:
-            res.append(heapq.heappop(heap)[1].word)
-        return res[::-1]
+    def __repr__(self):
+        return self.word + ":" + str(self.count)
+    
+class Solution:
+    def topKFrequent(self, words: list[str], k: int) -> list[str]:
+        counts =  Counter(words)
+        min_heap = []
+        for word, count in counts.items():
+            item = Item(count, word)
+            heapq.heappush(min_heap, item)
+            if len(min_heap) > k:
+                  heapq.heappop(min_heap)
+
+        ans = []
+        while min_heap:
+            ans.insert(0,heapq.heappop(min_heap).word)
+        return ans
+
